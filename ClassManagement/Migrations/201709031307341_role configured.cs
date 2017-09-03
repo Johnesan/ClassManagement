dc4dc3e9ee0dd9b8a7007d6f08182eedb87cb051
John Esan <jcoool40@gmail.com>
2017-09-03 16:33:06 +0100

    Default Admin role management functionality added

---
+++ ClassManagement/Migrations/201709031307341_role configured.cs
@@ -0,0 +1,16 @@
+namespace ClassManagement.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class roleconfigured : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- ClassManagement/Startup.cs
+++ ClassManagement/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using ClassManagement.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(ClassManagement.Startup))]
@@ -9,6 +12,30 @@ namespace ClassManagement
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateRolesAndUsers();
+        }
+        private void CreateRolesAndUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                var user = new ApplicationUser { FirstName = "John", LastName = "Esan", UserName = "jcool", Email = "jcoool40@gmail.com" };
+                string passwod = "ABC@123";
+                var createUser = userManager.Create(user, passwod);
+
+                if (createUser.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "Admin");
+                }
+            }
         }
     }
 }
